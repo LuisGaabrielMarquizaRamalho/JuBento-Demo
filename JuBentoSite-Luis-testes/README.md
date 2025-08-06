f389b4204c741d64c76998a5df65c334012ac3ef
Luis Marquiza <142032704+LuisGaabrielMarquizaRamalho@users.noreply.github.com>
2025-08-06 10:32:45 -0300

    Delete JuBentoSite-Luis-testes directory

--- JuBentoSite-Luis-testes/README.md
+++
@@ -1,2 +0,0 @@
-# JuBentoSite
-Um site dedicado para Juliano Bento e seus feitos
--- JuBentoSite-Luis-testes/assets/js/include.js
+++
@@ -1,9 +0,0 @@
-async function includeComponent(selector, file) {
-  const el = document.querySelector(selector);
-  const res = await fetch(file);
-  const html = await res.text();
-  el.innerHTML = html;
-}
-
-includeComponent("#header", "/components/header.html");
-includeComponent("#footer", "/components/footer.html");
